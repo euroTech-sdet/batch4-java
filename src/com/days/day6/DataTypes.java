2a56ad50f2fc89e78b60e51f2ca977f4218fc71c
tlh43 <talha43gs@gmail.com>
2022-05-03 20:58:59 +0100

    Day 6 Uploaded

---
+++ src/com/days/day6/DataTypes.java
@@ -0,0 +1,114 @@
+package com.days.day6;
+
+public class DataTypes {
+
+
+    public static void main(String[] args) {
+
+        boolean isTrue;
+
+        isTrue = true;
+
+        isTrue = false;
+
+        boolean isJavaCool = true;
+
+        System.out.println(isJavaCool);
+
+        //////////////////////////////////////////////////////////////////////
+
+        // STRING
+
+
+
+        String name = "Talha Ozleblebici" ;
+
+        System.out.println(name);
+
+        String chars = "€ 90.000 per year !!? :)" ;
+
+        System.out.println("chars = " + chars );
+
+        int numberOne_Int= 1;
+
+        double numberOne_Double= 1.0;
+
+        String numberOne_String = "1";
+
+        System.out.println("numberOne_Int = " + numberOne_Int);         // 1
+        System.out.println("numberOne_Double = " + numberOne_Double);   // 1.0 double
+        System.out.println("numberOne_String = " + numberOne_String);   // 1 string type!!!
+
+        System.out.println(  numberOne_Int + numberOne_Int );  // 2
+
+        System.out.println(  numberOne_String + numberOne_String ); //  11
+
+
+        String address = "22/b baker street London           Test         TEst";
+
+        System.out.println("address = " + address);
+
+
+        ///////////////////////////////////////////////////////
+
+        // char
+
+        char semicolon= ';' ;
+
+        System.out.println("semicolon = " + semicolon);
+
+        char beta = 'β';
+
+        System.out.println("beta = " + beta);
+
+        char character = 'A';
+
+        System.out.println("character = " + character);
+
+        //
+        char number1 = 976;
+
+        System.out.println("number1 = " + number1);
+
+      //
+
+        int age, bbp, ttr;
+
+        String namee = "Henry" , surname;
+
+        int agee, year,  classes;
+
+       // int num3; num4;
+
+        double tax,tax2;
+
+
+
+        int t1e11s1t1; // valid
+
+//        int 1test // NOT VALID
+
+        int test_no;  // NOT VALID , no spaces!!!
+
+        int test;
+//        int int; // NOT VALID!!! dont USE reserved keyword!!
+
+        String bookName= "dasd";
+
+
+        /////////////////////////////////////////////////////////////////
+
+        //CONSTANT VARIABLES
+
+        final double PI_NUMBER = 3.14;
+
+        final int TEST_VALUE = 1000;
+
+        System.out.println(TEST_VALUE); // u can call them, but can not re assign !!
+        // piNumber = 3.15; can not re assign !!! it is constant variable !!!
+
+
+    }
+
+
+}
