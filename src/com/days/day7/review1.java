62581e09096f47579c37fa1fe8f58228509319df
tlh43 <talha43gs@gmail.com>
2022-05-05 21:04:31 +0100

    Day 57 Review Uploaded

---
+++ src/com/days/day7/review1.java
@@ -0,0 +1,48 @@
+package com.days.day7;
+
+public class review1 {
+
+
+    public static void main(String[] args) {
+
+
+        int number = 1;
+
+        double fNumber = 1;
+
+        System.out.println("fNumber = " + fNumber);
+
+        fNumber=2;
+
+        System.out.println("fNumber = " + fNumber);
+
+        int negNumber= -1_231_231_231;
+
+        System.out.println("negNumber = " + negNumber);
+
+        System.out.println(1000_0000_0000_0000L);
+
+
+        ////////////////
+
+        int num=1;
+
+        System.out.println(num);
+
+        System.out.println("test"+4/2*3);
+
+
+        ///// TASK -4 Variables
+
+        //(Volume =Length ×Width ×Height)
+
+        double length = 4, width = 12, height = 11;
+
+        double volume = length * width * height;
+
+        System.out.println("volume = " + volume + " cm3");
+
+
+
+    }
+}
