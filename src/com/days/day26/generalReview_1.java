7033f9abdfb94fadf7f29bedf380e95cc6fd8ded
tlh43 <talha43gs@gmail.com>
2022-06-13 20:51:33 +0100

    Day 26 Uploaded

---
+++ src/com/days/day26/generalReview_1.java
@@ -0,0 +1,50 @@
+package com.days.day26;
+
+public class generalReview_1 {
+
+
+    public static void main(String[] args) {
+
+
+        int number = 3;
+
+        String str = number + "" + true;
+
+        System.out.println(str);  // "3"
+
+
+        String input = "1A23$%^cdA";
+
+
+        char keyword = 'A';
+
+        System.out.println(keyword + 1); // 66  Data Type conv for expressions!!
+        System.out.println(keyword); // A
+
+
+//        -------
+
+        double dbl = 3.99;
+
+        System.out.println((int) dbl); // 3
+
+
+        int number1 = 1;
+
+        double number1dbl = 1;
+
+        System.out.println("number1==number1dbl : " + (number1 == number1dbl));
+
+
+        byte b1= 2;
+        byte b2= 3;
+
+        byte b3= 126 + 1;
+
+       // byte b4= 128; //128 is out of byte range!!
+
+      //  byte b5 = b1 + b2; //int -> type promotion in expression!!!
+    }
+
+
+}
